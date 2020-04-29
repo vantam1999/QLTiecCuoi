406f0fae6096a8a69e9402f889ac61b6ec15e13b
vantam1999 <tvantam1475369@gmail.com>
2020-04-29 10:27:17 +0700

    update about hall

---
+++ qlTiecCuoi/Content/scripts/tabHall.js
@@ -0,0 +1,21 @@
+﻿window.addEventListener("DOMContentLoaded", () => {
+    var tabHall = document.querySelectorAll(".tab__link");
+    var carouselHall = document.querySelectorAll(".carousel-hall");
+    for (let i = 0; i < tabHall.length; i++) {        
+        tabHall[i].addEventListener("click", activeHall)
+    }
+    function activeHall(e) {
+        for (let i = 0; i < tabHall.length; i++) {
+            tabHall[i].classList.remove("active__tab");
+            carouselHall[i].classList.remove("active__carousel");            
+        }
+        // active tab carousel
+        var getTarget = e.target.getAttribute("target");
+        console.log(getTarget);
+        var getId = document.getElementById(getTarget);
+        getId.classList.add("active__carousel");
+
+        // active tab link
+        e.target.classList.add("active__tab");
+    }
+});
