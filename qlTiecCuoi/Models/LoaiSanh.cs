469e6080ccc5b1519005da52e510e7579a142d8f
= <=>
2020-06-03 20:01:45 +0700

    model

---
+++ qlTiecCuoi/Models/LoaiSanh.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace qlTiecCuoi.Models
+{
+    public class LoaiSanh
+    {
+        [Key]
+        public int IDLoaiSanh { get; set; }
+        public string TenLoaiSanh { get; set; }
+        public ICollection<SanhTiec> sanhtiecs { get; set; }
+    }
+}
---
+++ qlTiecCuoi/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace qlTiecCuoi.Models
+{
+    public class User
+    {
+        [Key]
+        public int IDUser { get; set; }
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string PassWord { get; set; }
+        [Required]
+        [NotMapped]
+        [DataType(DataType.Password)]
+        [Compare("PassWord", ErrorMessage ="Mật khẩu nhập lại không khớp")]
+        public string ConFirmPassWord { get; set; }
+        [Required]
+        public string SDT { get; set; }
+        public string DiaChi { get; set; }
+    }
+}
